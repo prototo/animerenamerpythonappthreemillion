7de3ced82e3adac739cc0262f8bc963ba4869e8c
prototo <gmackelden@gmail.com>
2014-02-16 20:15:51 +0000

    update

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ API_PORT = 9000
 
 # various data for api calls
 PROTOVER = 3  # this is the api version (I think)
-CLIENT_NAME = 'ARPATM'
+CLIENT_NAME = 'arpatm'
 CLIENT_VERSION = 0
 CLIENT_PORT = 1024  # don't know why I chose this
 
 # fake user data to try authenticating with
 uname = 'prototo'
-upass = 'prototo'
+upass = 'nope'
 
 # make a fake auth request
 msg = "AUTH user=%s&pass=%s&protover=%s&client=%s&clientver=%s" % (uname, upass, PROTOVER, CLIENT_NAME, CLIENT_VERSION)
@@ -31,13 +31,10 @@ sock.bind(('', CLIENT_PORT))
 # so we have to convert to/from and sending/receiving
 sock.sendto(bytes(msg, "utf-8"), (API_ADDR, API_PORT))
 
-# wait infinitely for something to come back on our socket and
-# get put in the data variable
-data = False
-while not data:
-  data, addr = sock.recvfrom(1400)  # buffer size of 1400 bytes
-  data = data.decode("utf-8")
-  print("got this back:\n", data)
+# recvfrom should block and we're only waiting for one packet
+data, addr = sock.recvfrom(1400)  # buffer size of 1400 bytes
+data = data.decode("utf-8")
+print("got this back:\n", data)
 
 # yey
 exit(0)
