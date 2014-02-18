a0ce1a78b96de0a7983c8cdb6da3ab88977e2bf2
prototo <gmackelden@gmail.com>
2014-02-18 20:57:02 +0000

    fixed ed2k so it actually works

--- ed2k.py
+++ ed2k.py
@@ -2,20 +2,21 @@
 
 import hashlib
 
-def hash(filename, chunk_size=9500)
+def hash(filename, chunk_size=9500):
   # the ed2k hash object
   ed2k_hash = hashlib.new("md4")
 
-  with open(filename, "r") as f:
+  with open(filename, "rb") as f:
     while True:
       # read a chunk_size chunk of the file
       chunk = f.read(1024 * chunk_size)
       if chunk:
         # calculate the md4 for the chunk and update the ed2k hash
         hash = hashlib.new("md4", chunk).hexdigest()
-        ed2k_hash.update(hash)
+        ed2k_hash.update(hash.encode("utf-8"))
       else:
         break
 
   # return the ed2k hash string
   return ed2k_hash.hexdigest()
+
