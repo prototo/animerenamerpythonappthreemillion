fdefaa1a44d877ce43887309b201ff13afc484d3
prototo <gmackelden@gmail.com>
2014-05-20 15:45:39 +0100

    Fixed up image links for anime template

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+import os.path
 
+from config import image_store
 from animeinfo import Anime
 
 app = Flask(__name__)
+
+@app.route('/images/<path:filename>')
+def images(filename):
+  name = os.path.basename(filename)
+  path = image_store.strip() + name.strip()
+  return send_file(path)
 
 @app.route('/anime/<aid>')
 def anime(aid):
