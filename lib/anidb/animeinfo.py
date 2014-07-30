34a5533cb7594997cb7f9e065f25de2cee6bd086
prototo <gmackelden@gmail.com>
2014-07-30 02:00:30 +0100

    FUCK YOU GIT

---
+++ lib/anidb/animeinfo.py
@@ -0,0 +1,244 @@
+from config import *
+from urllib.parse import urlencode
+from urllib.request import urlretrieve
+import httplib2
+import os.path
+import xml.etree.ElementTree as etree
+import lib.index as index
+import datetime
+
+def get_date(str):
+	try:
+		d = datetime.datetime.strptime(str, '%Y-%m-%d')
+		return d
+	except:
+		return None
+
+class Anime:
+	xml = None #raw utf-8 string
+	root = None #root element
+	aid = None
+
+	def __init__(self, aid):
+		self.aid = aid
+		self.xml = AnimeFetcher(aid).getXML()
+
+		# no XML? AnimeFetcher failed but couldn't do anything about it due to poor design
+		# raise noSuchAnimeError somewhere?
+		if self.xml is None:
+			return None
+		self.root = etree.fromstring(self.xml)
+
+		if self.index_anime():
+			self.index_episodes()
+
+	def getType(self):
+		type = self.root.find('type')
+		return self.elementText(type)
+
+	def getEpisodecount(self):
+		episodecount = self.root.find('episodecount')
+		return self.elementText(episodecount)
+
+	def getStartdate(self):
+		startdate = self.root.find('startdate')
+		return self.elementText(startdate)
+
+	def getEndDate(self):
+		enddate = self.root.find('enddate')
+		return self.elementText(enddate)
+
+	def getTitle(self):
+		dict = {}
+		titles = self.root.find('titles')
+		if titles is None:
+			return None
+		for title in titles:
+			dict[title.get('{http://www.w3.org/XML/1998/namespace}lang')] = title.text
+		return dict
+
+	def getDescription(self):
+		description = self.root.find('description')
+		return self.elementText(description)
+
+	# images pulled to image_store/aid.jpg
+	def getPicture(self):
+		path = image_store + str(self.aid) + '.jpg'
+
+		# check locally first
+		if os.path.isfile(path):
+			print("Picture: we got it")
+			return path
+
+		# gonna have to be the internets
+		base = 'http://img7.anidb.net/pics/anime/'
+		location = self.root.find('picture')
+
+		if location is None:
+			return None
+		else:
+			try:
+				#print('urllib.request.urlretrieve({0}{1})'.format(base, location.text))
+				path, headers = urlretrieve(base+location.text, path)
+				return path
+			except:
+				print("Who knows... No pictures though")
+				return None
+
+
+	def getCategories(self):
+		#dict = {}
+		list = []
+		categories = self.root.find('categories')
+		if categories is None:
+			return None
+		for category in categories:
+			name = category.find('name')
+			#desc = category.find('description')
+			#dict[name.text] = category.texts
+			list.append(name.text)
+		return list
+
+	# build a dictionary of {ep { id, airdate, length, titles {lang, lang 2}}}
+	def getEpisodes(self):
+		a = {}
+		episodes = self.root.find('episodes')
+		if episodes is None:
+			return None
+		for episode in episodes:
+			epno = episode.find('epno').text
+			a[epno] = {}
+			dict = {}
+			if episode.find('epno').get('type') == '1':
+				a[epno]['id'] = episode.get('id')
+				#print(episode.get('id'))
+				airdate = episode.find('airdate')
+				a[epno]['airdate'] = airdate.text if airdate != None else None
+				#sprint(episode.find('airdate').text)
+				a[epno]['length'] = episode.find('length').text
+				#print(episode.find('length').text)
+				titles = episode.findall('title')
+				for title in titles:
+					dict[title.get('{http://www.w3.org/XML/1998/namespace}lang')] = title.text
+				a[epno]['title'] = dict
+
+		return a
+
+	#
+	def elementText(self, element):
+		if element is None:
+			return None
+		else:
+			return element.text
+
+	# index epiosdes from HTTP API XML data
+	def index_episodes(self):
+		episodes = self.getEpisodes()
+
+		for key in list(episodes.keys()):
+			episode = episodes.get(key, None)
+			if not episode:
+				continue
+
+			titles = episode.get('title')
+			aired_date = get_date(episode.get('airdate', None))
+
+			index.add_episode(
+				id=episode.get('id'), aid=self.aid,
+				epno=key,
+				title=titles.get('en', None),
+				title_ro=titles.get('x-jat', None),
+				title_jp=titles.get('ja', None),
+				aired_date=aired_date
+			)
+
+		return True
+
+	# index anime from HTTP API XML data
+	def index_anime(self):
+		# if we've already indexed this anime, return
+		# TODO: Just load this object from that data we've already stored rather than doing the API request
+		if index.get_anime(self.aid) != None:
+			return False
+
+		names = self.getTitle()
+		name = names.get('en', None)
+		name_ro = names.get('x-jat', None)
+		name_jp = names.get('ja', None)
+		start_date = get_date(self.getStartdate())
+		end_date = get_date(self.getEndDate())
+
+		index.add_anime(
+			id=self.aid, name=name, name_ro=name_ro, name_jp=name_jp,
+			episode_count=self.getEpisodecount(),
+			description=self.getDescription(),
+			picture='', # self.getPicture(),
+			start_date=start_date,
+			end_date=end_date
+		)
+
+		return True
+
+class AnimeFetcher:
+	aid = None
+	xml = None
+	root = None
+	path = None
+
+	def __init__(self, aid):
+		self.aid = aid
+		self.path = os.path.join(data_store, str(aid) + ".xml")
+		self.loadXML()
+
+	# return utf-8 string representation
+	def getXML(self):
+		return self.xml
+
+	# if aid.xml present read into self.xml, else DOWNLOAD
+	def loadXML(self):
+		if os.path.isfile(self.path):
+			print("Found it!")
+			with open(self.path, mode='r', encoding='utf-8') as file:
+				self.xml = file.read()
+		else:
+			print("Nope.  Downloading")
+			self.downloadXML(self.getAddress())
+
+		# if at this point self.xml isn't set, something failed
+
+	# create/overwrite aid.xml
+	def storeXML(self, xml):
+		with open(self.path, mode='w', encoding='utf-8') as file:
+			file.write(self.xml)
+		print("Stored")
+
+	# download contents of specified url, response returned as string
+	# content is bytes
+	def downloadXML(self, url):
+		h = httplib2.Http('.cache')
+		response, content = h.request(url)
+		# api responds with a single <error> element incase of error
+		temp_xml = content.decode()
+		root = etree.fromstring(temp_xml)
+		if root.tag == 'error':
+			print("That didn't work: " + root.text)
+			return None
+
+		self.xml = temp_xml
+		self.storeXML(self.xml)
+
+	# return request url as string
+	def getAddress(self):
+		base = "http://api.anidb.net:9001/httpapi?"
+		params = {
+			"client": HTTP_CLIENT_NAME,
+			"clientver": HTTP_CLIENT_VERSION,
+			"protover": HTTP_PROTOVER,
+			"request": "anime",
+			"aid": self.aid
+		}
+		request = "{0}{1}".format(base, urlencode(params))
+
+		print("Full request: " + request)
+		return request
+
---
+++ lib/index.py
@@ -0,0 +1,188 @@
+from sqlalchemy import create_engine, Column, ForeignKey, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, joinedload
+
+from contextlib import contextmanager
+
+dbpath = ':memory:'
+dbpath = 'anime.db'
+
+engine = create_engine('sqlite:///' + dbpath)
+
+Base = declarative_base()
+
+"""
+    Files table
+"""
+class File(Base):
+  __tablename__ = 'files'
+
+  ed2k = Column(String, primary_key=True, unique=True)
+  path = Column(String)
+  aid = Column(Integer, ForeignKey('anime.id'))
+  eid = Column(Integer, ForeignKey('episodes.id'))
+  
+  def __repr__(self):
+    return self.path
+
+"""
+    Episodes table
+"""
+class Episode(Base):
+  __tablename__ = 'episodes'
+
+  id = Column(Integer, primary_key=True, unique=True)
+  aid = Column(Integer, ForeignKey('anime.id'))
+  epno = Column(Integer)
+  title = Column(String)
+  title_ro = Column(String)
+  title_jp = Column(String)
+  aired_date = Column(Date)
+
+  files = relationship("File", backref="episode")
+
+  def __repr__(self):
+    return self.get_title()
+
+  def get_title(self):
+    return self.title or self.title_ro or self.title_jp
+
+  def get_titles(self):
+    titles = [self.title, self.title_ro, self.title_jp]
+    return [ title for title in titles if title ]
+
+"""
+    Anime table
+"""
+class Anime(Base):
+  __tablename__ = 'anime'
+
+  id = Column(Integer, primary_key=True, unique=True)
+  name = Column(String)
+  name_ro = Column(String)
+  name_jp = Column(String)
+  episode_count = Column(Integer)
+  description = Column(String)
+  picture = Column(String)
+  start_date = Column(Date)
+  end_date = Column(Date)
+
+  files = relationship("File", order_by=File.path, backref="anime")
+  episodes = relationship("Episode", order_by=Episode.epno, backref="anime")
+
+  def __repr__(self):
+    return self.get_name()
+
+  def get_name(self):
+    return self.name or self.name_ro or self.name_jp
+
+  def get_names(self):
+      names = [self.name, self.name_ro, self.name_jp]
+      return [ name for name in names if name ]
+
+"""
+    Downloads table
+"""
+class Download(Base):
+    __tablename__ = 'downloads'
+
+    id = Column(Integer, primary_key=True)
+    eid = Column(Integer, ForeignKey('episodes.id'))
+
+    episode = relationship("Episode", backref="download")
+
+# create all the tables that haven't been created
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine, expire_on_commit=False)
+
+@contextmanager
+def session_scope():
+  session = Session()
+  try:
+    yield session
+    session.commit()
+  except:
+    session.rollback()
+    raise
+  finally:
+    session.close()
+
+# check a row exists in the database for
+def exists(model, attribute, value):
+  with session_scope() as session:
+    q = session.query(model).filter(attribute == value)
+    return session.query(q.exists()).scalar()
+
+# check a filepath has already been indexed
+def file_path_exists(filepath):
+  return exists(File, File.path, filepath)
+
+# extract a subset of keys from the given dict
+def extract_keys(d, keys):
+  return { key: d.get(key, None) for key in keys }
+
+# add a row to a table in the database
+def add(model, data):
+  with session_scope() as session:
+    item = model(**data)
+    session.add(item)
+
+# delete a row from the given table
+def delete(model, attribute, value):
+  with session_scope() as session:
+    q = session.query(model).filter(attribute == value).first()
+    if q:
+      session.delete(q)
+
+# add a file into the files tables
+def add_file(**kwargs):
+  keys = ['path', 'ed2k', 'eid', 'aid']
+  data = extract_keys(kwargs, keys)
+  if (file_path_exists(data['path'])):
+    return False
+  add(File, data)
+
+  # remove any rows for this episode from the downloads table
+  delete(Download, Download.eid, data['eid'])
+
+  print("file", data['path'])
+
+# add an episode into the episodes table
+def add_episode(**kwargs):
+  keys = ['id', 'aid', 'epno', 'title', 'title_ro', 'title_jp', 'aired_date']
+  data = extract_keys(kwargs, keys)
+  if (exists(Episode, Episode.id, kwargs['id'])):
+    return False
+  add(Episode, data)
+  # print("episode", kwargs['title'] or kwargsp['title_ro'] or kwargs['title_jp'])
+
+def get_episode(eid):
+    with session_scope() as session:
+        q = session.query(Episode).options(joinedload('*')).filter(Episode.id == eid).first()
+        return q
+
+# add an anime into the anime table
+def add_anime(**kwargs):
+  keys = ['id', 'name', 'name_ro', 'name_jp', 'episode_count', 'description', 'picture', 'start_date', 'end_date']
+  data = extract_keys(kwargs, keys)
+  if (exists(Anime, Anime.id, kwargs['id'])):
+    return False
+  add(Anime, data)
+  # print("anime", kwargs['name'] or kwargs['name_ro'] or kwargs['name_jp'])
+
+def get_anime(aid):
+  with session_scope() as session:
+    q = session.query(Anime).options(joinedload('*')).filter(Anime.id == aid).first()
+    return q
+
+def get_all_anime():
+  with session_scope() as session:
+    q = session.query(Anime).options(joinedload('*')).all()
+    return q
+
+# add a download to the downloads table
+def add_download(**kwargs):
+    keys = ['eid']
+    data = extract_keys(kwargs, keys)
+    # don't really care if one already exists
+    add(Download, data)
