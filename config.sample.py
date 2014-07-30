6572f5f302742f0b4c76d21e07c1a2b7d02e5b45
prototo <gmackelden@gmail.com>
2014-07-30 02:08:32 +0100

    Added sample config file

---
+++ config.sample.py
@@ -0,0 +1,32 @@
+#!/bin/env python
+
+"""
+  USER DEFINED STUFF
+"""
+username = 'username'
+password = 'password'
+store = '/media/anime'
+data_store = '.data'
+image_store = '.images'
+torrent_watch_dir = '/media/watch'
+
+"""
+  DONT CHANGE THIS STUFF
+"""
+# api location
+API_ADDR = 'api.anidb.net'
+API_PORT = 9000
+
+# various data for api calls
+PROTOVER = 3  # this is the api version (I think)
+CLIENT_NAME = 'arpatm'
+CLIENT_VERSION = 0
+CLIENT_PORT = 1024  # don't know why I chose this
+
+HTTP_CLIENT_NAME = 'harpatm'
+HTTP_CLIENT_VERSION = 0
+HTTP_PROTOVER = 1
+
+# TVDB config
+TVDB_KEY = '2EAC4B8F6B6CF146'
+
